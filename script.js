16a42b20a39cf1f5758cd61c92af97e2cf385b03
BinaryBytecode <binarybytecode@proton.me>
2023-10-28 19:47:54 +0200

    Created Repo

---
+++ script.js
@@ -0,0 +1,39 @@
+function onload() {
+  new TypeIt("#typer", {
+    speed: 50,
+    startDelay: 900,
+    loop: true,
+  })
+    .type("Hello, I'm BinaryBytecode", { delay: 100 })
+    .type(", I'm a <span class='place'>web developper</span>", { delay: 100  })
+    .delete(".place", { delay: 100  })
+    .type("<span class='place'>malware developper</span>", { delay: 100  })
+    .delete(".place", { delay: 100  })
+    .type("<span class='place'>hacker</span>", { delay: 100  })
+    .delete(".place", { delay: 100  })
+    .delete()
+    .pause(500)
+    .type("Hello, I'm CreepinGenius", { delay: 100 })
+    .type(", I'm a <span class='place'>minecraft player</span>", { delay: 100  })
+    .delete(".place", { delay: 100  })
+    .type("<span class='place'>figura scripter</span>", { delay: 100  })
+    .go();
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
